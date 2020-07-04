038768bb2a4ff16d1d037e43cbfc0a9322750869
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-04 21:59:32 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -7,3 +7,16 @@ https://stackoverflow.com/questions/44297803/what-is-the-role-of-the-package-loc
 /**
  https://stackoverflow.com/questions/7137397/module-exports-vs-exports-in-node-js
  */
+
+ function main(){
+     let cycles= 1000000000;
+     let start= Date.now();
+     for(let i=0;i<cycles;i++){
+
+     }
+     let end= Date.now();
+     let duration= ((end-start)/1000);
+     console.log("JavaScript looped %d times in %d seconds", cycles, duration);
+ }
+
+ main();
